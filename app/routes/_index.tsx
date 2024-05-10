2028ecdd829529860b03945a3fc92263cc0c4790
iamyahia <yahiabaiz@gmail.com>
2024-05-10 22:04:20 +0300

    change navigation layouts

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
-import { useState } from "react";
-import Sidebar from "~/components/Sidebar/Sidebar";
+import { Outlet } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,22 +9,9 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
-  const [sideMenuIsExpand, setSideMenuIsExpand] = useState(true);
-
   return (
-    <div className="relative min-h-screen md:flex">
-      <Sidebar setExpand={setSideMenuIsExpand} />
-
-      <div
-        className={`flex-1 min-h-screen mx-0 bg-slate-100 transition-all duration-300 ease-in-out ${
-          sideMenuIsExpand ? "md:ml-72" : "md:ml-20"
-        }`}
-      >
-        <div>
-          <h1>hi</h1>
-          <h4>by</h4>
-        </div>
-      </div>
+    <div>
+      <Outlet />
     </div>
   );
 }
---
+++ app/routes/_navigation.tsx
@@ -0,0 +1,21 @@
+import { Outlet } from "@remix-run/react";
+import { useState } from "react";
+import Sidebar from "~/components/Sidebar/Sidebar";
+
+export default function NavigationLayout() {
+  const [sideMenuIsExpand, setSideMenuIsExpand] = useState(true);
+
+  return (
+    <div className="relative min-h-screen md:flex">
+      <Sidebar setExpand={setSideMenuIsExpand} />
+
+      <div
+        className={`flex-1 min-h-screen mx-0 bg-slate-100 transition-all duration-300 ease-in-out ${
+          sideMenuIsExpand ? "md:ml-72" : "md:ml-20"
+        }`}
+      >
+        <Outlet />
+      </div>
+    </div>
+  );
+}
