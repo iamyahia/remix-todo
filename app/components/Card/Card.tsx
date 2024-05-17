8310b5c2af3d64d0e6dd66c1063da5c0545cdb0e
iamyahia <yahiabaiz@gmail.com>
2024-05-17 13:36:38 +0300

    remove unnecessary stuf

--- app/components/Card/Card.tsx
+++ app/components/Card/Card.tsx
@@ -33,19 +33,6 @@ function CardBox({}: CardBoxProps) {
     },
   ];
 
-  /**
-   * 
-   * <div class="flex flex-wrap">
-  <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500"></div>
-  <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-400"></div>
-  <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-500"></div>
-  <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6 mb-4 bg-gray-400"></div>
-  <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/2 xl:w-1/6 mb-4 bg-gray-500"></div>
-  <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/2 xl:w-1/6 mb-4 bg-gray-400"></div>
-</div>
-   * 
-   */
-
   return (
     <div className="flex flex-wrap m-7 ">
       {cards.map((card, index) => (
