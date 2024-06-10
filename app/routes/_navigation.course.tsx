72515f80e75d2422b7e45d201665b9c6d4ace633
iamyahia <yahiabaiz@gmail.com>
2024-06-10 17:25:52 +0300

    Add(table): adding basic structure for table

--- app/routes/_navigation.course.tsx
+++ app/routes/_navigation.course.tsx
@@ -1,3 +1,138 @@
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { useState } from "react";
+
+type Person = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: number;
+  enrollNumber: string;
+  dateOfAdmission: string;
+};
+
+function generateUniqueRandomNumber() {
+  const timestamp = Date.now(); // Current timestamp in milliseconds
+  const randomNum = Math.floor(Math.random() * 1000); // Random number between 0 and 999
+  return timestamp.toString() + randomNum.toString();
+}
+const defaultData: Person[] = [
+  {
+    firstName: "tanner",
+    lastName: "linsley",
+    email: "tanner@gmail.com",
+    phone: 964000000000,
+    enrollNumber: generateUniqueRandomNumber(),
+    dateOfAdmission: "08-Dec, 2021",
+  },
+  {
+    firstName: "tandy",
+    lastName: "miller",
+    email: "tandy@gmail.com",
+    phone: 964000000000,
+    enrollNumber: generateUniqueRandomNumber(),
+    dateOfAdmission: "08-Dec, 2021",
+  },
+  {
+    firstName: "joe",
+    lastName: "dirte",
+    email: "joe@gmail.com",
+    phone: 964000000000,
+    enrollNumber: generateUniqueRandomNumber(),
+    dateOfAdmission: "08-Dec, 2021",
+  },
+];
+
+const columnHelper = createColumnHelper<Person>();
+
+const columns = [
+  columnHelper.accessor((row) => row.firstName + " " + row.lastName, {
+    id: "firstName",
+    cell: (info) => info.getValue(),
+    header: "Full Name",
+  }),
+  columnHelper.accessor("email", {
+    cell: (info) => info.getValue(),
+    header: "Full Name",
+  }),
+  columnHelper.accessor("phone", {
+    header: "Phone",
+  }),
+  columnHelper.accessor("enrollNumber", {
+    header: "Enrollment Number",
+  }),
+  columnHelper.accessor("dateOfAdmission", {
+    header: "Date of Admission",
+  }),
+];
+
 export default function Course() {
-  return <div>Course</div>;
+  const [data, setData] = useState(() => [...defaultData]);
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+  return (
+    <div className="container mx-auto p-4">
+      <div className="overflow-x-auto">
+        <table className="min-w-full bg-white border border-gray-200">
+          <thead className="bg-gray-50">
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <th
+                    className="px-4 py-2 border-b border-gray-200"
+                    key={header.id}
+                  >
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {table.getRowModel().rows.map((row) => (
+              <tr className="hover:bg-gray-100" key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <td
+                    className="px-4 py-2 border-b border-gray-200"
+                    key={cell.id}
+                  >
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
+            {table.getFooterGroups().map((footerGroup) => (
+              <tr key={footerGroup.id}>
+                {footerGroup.headers.map((header) => (
+                  <th key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.footer,
+                          header.getContext()
+                        )}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </tfoot>
+        </table>
+      </div>
+    </div>
+  );
 }
